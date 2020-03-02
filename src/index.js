13e3c5c41957cd63ba860a8248b75a93cb355a66
starff <starff.st@gmail.com>
2020-03-02 21:51:17 +0300

    feat: add first step

--- src/index.js
+++ src/index.js
@@ -4,7 +4,46 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  let arr = expr.split('').filter(item => item != '');
+    let rightExpr = expr.replace(/\s/g, ""); // избавился от пробелов
+    let bracketCounter = 0;
+    
+    
+    // проверка на скобки
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i] === '(') {
+        bracketCounter++;
+      };
+      if(arr[i] === ')') {
+        bracketCounter--;
+      }
+    };
+    if(bracketCounter !== 0) {
+      throw new Error ('ExpressionError: Brackets must be paired');
+    }
+
+
+
+    let result = 0;
+    arr.forEach((item, index) => {
+      if (index == 0) {
+        result = result + +arr[0];
+      } else if (item == '+') {
+        result = result + +arr[index +  1];
+      } else if (item == '-') {
+        result = result - +arr[index + 1];
+      } else if (item == '*') {
+        result = result * +arr[index + 1];
+      } else if (item == '/') {
+        if (arr[index + 1] == 0) {
+          throw new TypeError ('TypeError: Division by zero.');
+        } else {
+          result = result / +arr[index + 1];
+        };
+      }
+    });
+
+    return result;
 }
 
 module.exports = {
