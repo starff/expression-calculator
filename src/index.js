fe004059779c6c876ad5234ebad39e3acb19e808
starff <starff.st@gmail.com>
2020-03-03 01:40:58 +0300

    feat: add new features

--- src/index.js
+++ src/index.js
@@ -5,11 +5,8 @@ function eval() {
 
 function expressionCalculator(expr) {
   let arr = expr.split('').filter(item => item != '');
-    let rightExpr = expr.replace(/\s/g, ""); // избавился от пробелов
-    let bracketCounter = 0;
-    
-    
     // проверка на скобки
+    let bracketCounter = 0;
     for(let i = 0; i < arr.length; i++) {
       if(arr[i] === '(') {
         bracketCounter++;
@@ -22,26 +19,61 @@ function expressionCalculator(expr) {
       throw new Error ('ExpressionError: Brackets must be paired');
     }
 
+    // избавился от пробелов
+    let rightExpr = expr.replace(/\s/g, ""); 
 
+    //функции вычисления
+    function sum(express) {
+      let items = express.split('+');
+      if(items.length > 1) {
+        return +items[0] + +items[1];
+      };
+    };
+    function minus(express) {
+      let items = express.split('-');
+      if(items.length > 1) {
+        return +items[0] - +items[1];
+      };
+    };
+    function mult(express) {
+      let items = express.split('*');
+      if(items.length > 1) {
+        return +items[0] * +items[1];
+      };
+    };
+    function div(express) {
+      let items = express.split('/');
+      if(items.length > 1) {
+        return +items[0] / +items[1];
+      };
+    };
+    function count(arr) {
+      arr = arr.replace(/\b\d{0,}\*\b\d{0,}\b/g, mult);
+      arr = arr.replace(/\b\d{0,}\/\b\d{0,}\b/g, div);
+      arr = arr.replace(/\b\d{0,}\-\b\d{0,}\b/g, minus);
+      arr = arr.replace(/\b\d{0,}\+\b\d{0,}\b/g, sum);
+      return +arr;
+    };
 
-    let result = 0;
-    arr.forEach((item, index) => {
-      if (index == 0) {
-        result = result + +arr[0];
-      } else if (item == '+') {
-        result = result + +arr[index +  1];
-      } else if (item == '-') {
-        result = result - +arr[index + 1];
-      } else if (item == '*') {
-        result = result * +arr[index + 1];
-      } else if (item == '/') {
-        if (arr[index + 1] == 0) {
-          throw new TypeError ('TypeError: Division by zero.');
-        } else {
-          result = result / +arr[index + 1];
-        };
-      }
-    });
+    let bracketResult = rightExpr.replace(/\(.*\)/g, count);
+    let result = count(bracketResult);
+    // arr.forEach((item, index) => {
+    //   if (index == 0) {
+    //     result = result + +arr[0];
+    //   } else if (item == '+') {
+    //     result = result + +arr[index +  1];
+    //   } else if (item == '-') {
+    //     result = result - +arr[index + 1];
+    //   } else if (item == '*') {
+    //     result = result * +arr[index + 1];
+    //   } else if (item == '/') {
+    //     if (arr[index + 1] == 0) {
+    //       throw new TypeError ('TypeError: Division by zero.');
+    //     } else {
+    //       result = result / +arr[index + 1];
+    //     };
+    //   }
+    // });
 
     return result;
 }
